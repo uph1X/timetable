9182a68c7bd65ceafe3b50a54ce21b583d429d03
uph1X <ugriumov.ka@phystech.edu>
2022-12-05 16:54:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,6 @@ from datetime import date
 import calendar
 
 
-
-bot = telebot.TeleBot('5644571180:AAHF_bxgRFh3T9Hfs9i9onNhlz20-jF7bR4')
-@bot.message_handler(commands=['start'])
-
-def send_welcome(message):
-    stic = open('C:/Users/kfify/Downloads/TelegramBot/applecatrun-apple-cat.webp', 'rb') #при скачивании GIF-файла здесь нужно указать путь на него на своём устройстве
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    but1 = types.KeyboardButton("Вывести расписание на сегодня")
-    markup.add(but1)
-
-    bot.reply_to(message, "Привет, {0.first_name}\nДавай посмотрим, чем я могу быть полезен".format(message.from_user)
-  ,parse_mode='html',reply_markup=markup)
-    bot.send_sticker(message.chat.id,stic)
-import telebot
-from telebot import types
-import random
-import xlrd
-from datetime import date
-import calendar
-
-
 bot = telebot.TeleBot('5644571180:AAHF_bxgRFh3T9Hfs9i9onNhlz20-jF7bR4')
 @bot.message_handler(commands=['start'])
 
